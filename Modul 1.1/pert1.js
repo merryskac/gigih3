172fc603d54d320ad6e1e77f7a783acbda9d8d4d
unknown <merryskachr@gmail.com>
2023-07-05 11:12:33 +0800

    add showcase modul 1.1

---
+++ Modul 1.1/pert1.js
@@ -0,0 +1,32 @@
+const akun = [
+  {email: "test@gmail.com",
+  pass: "abcdefg"},
+  {email: "halo@gmail.com",
+  pass: "123456"}
+]
+
+const checkEmail = (email)=>{
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+const seestatus = ()=>{
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+
+  if(!email || !pass){
+    console.log("halo")
+    document.getElementById("status").innerHTML = "Email dan pass masih kosong";
+    return
+  }
+  if(!checkEmail(email)){
+    document.getElementById("status").innerHTML = "Email tidak valid";
+    return
+  }
+  if(pass.length <6){
+    document.getElementById("status").innerHTML = "Password harus lebih dari 6 karakter";
+    return
+  }
+
+  document.getElementById("status").innerHTML = "Email dan password valid!";
+}
